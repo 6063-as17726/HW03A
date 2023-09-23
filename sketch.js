1005158368d219672371faed024796b6e05a40a7
Ayushi Shah <as17726@nyu.edu>
2023-09-23 10:33:35 -0400

    Updates to Attempt 2

--- sketch.js
+++ sketch.js
@@ -19,27 +19,14 @@ function draw() {
     }
   }
   //big ellipse
-  //for(xpos = 0; xpos<=windowWidth;xpos+=4*maxdiam){
-    //if(xpos%2==0){
-      //for(ypos = 0; ypos<=windowHeight;ypos+=4*maxdiam){ 
-        //ellipse(xpos,ypos,maxdiam,maxdiam);
-      //}
-    //}
-    //else{
-    // for(xpos = 20; xpos<=windowHeight;xpos+=2*maxdiam){ 
-      // ellipse(xpos,ypos,maxdiam,maxdiam);
-      //}
-    //}
-  //}
   for(xpos = 0; xpos<=windowWidth;xpos+=2*maxdiam){
+    x+=1;
     if(x%2==0){
-      x+=1;
       for(ypos = 0; ypos<=windowHeight;ypos+=4*maxdiam){ 
         ellipse(xpos,ypos,maxdiam,maxdiam);
       }
     }
     else{
-      x+=1;
       for(ypos = 40; ypos<=windowHeight;ypos+=4*maxdiam){ 
         ellipse(xpos,ypos,maxdiam,maxdiam);
       }
