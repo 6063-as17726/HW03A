4c1efc41ca734d478bb1e201a4bc648ff1b40627
Ayushi Shah <as17726@nyu.edu>
2023-09-23 10:29:35 -0400

    Attempt 2

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@
 //let ypos = 0;
 let mindiam = 10;
 let maxdiam = 20;
+let x = 0;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
@@ -11,31 +12,37 @@ function draw() {
   background(255,219,88);
   fill("black");
 
-//small ellipse
-for(xpos = 0; xpos<=windowWidth;xpos+=4*mindiam)
-{
-  for(ypos = 0; ypos<=windowHeight;ypos+=4*mindiam)
-{ 
-  ellipse(xpos,ypos,mindiam,mindiam);
-}
-}
-//big ellipse
-for(xpos = 0; xpos<=windowWidth;xpos+=4*maxdiam)
-{
-  if(xpos%2<=1)
-  {
-  for(ypos = 0; ypos<=windowHeight;ypos+=4*maxdiam)
-{ 
-  ellipse(xpos,ypos,maxdiam,maxdiam);
-}
+  //small ellipse
+  for(xpos = 0; xpos<=windowWidth;xpos+=4*mindiam){
+    for(ypos = 0; ypos<=windowHeight;ypos+=4*mindiam){ 
+      ellipse(xpos,ypos,mindiam,mindiam);
+    }
   }
-  else
-  {for(ypos = 20; ypos<=windowHeight;ypos+=2*maxdiam)
-    { 
-      ellipse(xpos,ypos,maxdiam,maxdiam);
+  //big ellipse
+  //for(xpos = 0; xpos<=windowWidth;xpos+=4*maxdiam){
+    //if(xpos%2==0){
+      //for(ypos = 0; ypos<=windowHeight;ypos+=4*maxdiam){ 
+        //ellipse(xpos,ypos,maxdiam,maxdiam);
+      //}
+    //}
+    //else{
+    // for(xpos = 20; xpos<=windowHeight;xpos+=2*maxdiam){ 
+      // ellipse(xpos,ypos,maxdiam,maxdiam);
+      //}
+    //}
+  //}
+  for(xpos = 0; xpos<=windowWidth;xpos+=2*maxdiam){
+    if(x%2==0){
+      x+=1;
+      for(ypos = 0; ypos<=windowHeight;ypos+=4*maxdiam){ 
+        ellipse(xpos,ypos,maxdiam,maxdiam);
+      }
+    }
+    else{
+      x+=1;
+      for(ypos = 40; ypos<=windowHeight;ypos+=4*maxdiam){ 
+        ellipse(xpos,ypos,maxdiam,maxdiam);
+      }
     }
   }
 }
-
-}
-
